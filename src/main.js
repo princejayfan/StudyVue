f8444a175f74f9b8f41a33d7b99b974dc6967a63
yuefj <123456>
2018-10-10 22:42:49 +0800

    更改

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import Vue from 'vue';
-import ElementUI from 'element-ui';
 import router from './router';
+import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 
 
 
 Vue.use(ElementUI);
-Vue.use(router);
 new Vue({
   el: '#app',
   router,
